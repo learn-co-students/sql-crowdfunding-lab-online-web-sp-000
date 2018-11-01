96bedc9d60aae89b76330959356b3733172f7680
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-01 15:55:47 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,61 @@
+INSERT INTO users (name, age) VALUES ('greg', 50);
+INSERT INTO users (name, age) VALUES ('old greg', 100);
+INSERT INTO users (name, age) VALUES ('woody', 23);
+INSERT INTO users (name, age) VALUES ('jess', 76);
+INSERT INTO users (name, age) VALUES ('tim', 5);
+INSERT INTO users (name, age) VALUES ('tom', 45);
+INSERT INTO users (name, age) VALUES ('lisa', 13);
+INSERT INTO users (name, age) VALUES ('hardy', 59);
+INSERT INTO users (name, age) VALUES ('chad', 23);
+INSERT INTO users (name, age) VALUES ('brian', 93);
+INSERT INTO users (name, age) VALUES ('julia', 32);
+INSERT INTO users (name, age) VALUES ('arnold', 45);
+INSERT INTO users (name, age) VALUES ('tim', 50);
+INSERT INTO users (name, age) VALUES ('latrell', 40);
+INSERT INTO users (name, age) VALUES ('prince', 34);
+INSERT INTO users (name, age) VALUES ('derick', 32);
+INSERT INTO users (name, age) VALUES ('melissa', 83);
+INSERT INTO users (name, age) VALUES ('stephen', 43);
+INSERT INTO users (name, age) VALUES ('bob', 41);
+INSERT INTO users (name, age) VALUES ('rob zombie', 1000);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the fish', 'good stuff', 10000, '7/13/2018', '8/13/2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the trees', 'good stuff', 50000, '9/13/2018', '10/13/2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the air', 'better stuff', 50000000, '12/13/2018', '1/13/2019');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the grass', 'swell stuff', 430000, '5/20/2020', '8/13/2028');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the animals', 'swell stuff', 3524524, '2/1/2018', '8/1/2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the humans', 'good stuff', 43442342, '3/1/2018', '4/13/2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the animals and humans', 'hybrid stuff', 74567467, '6/11/2018', '10/13/2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the children', 'unreal stuff', 5635463, '7/13/2018', '8/13/2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the saved', 'awesome stuff', 1, '9/10/2018', '12/13/2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('save the airplanes', 'unreal stuff', 5635567563456, '1/13/2018', '12/13/2018');
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 40.00, 1, 4),
+(4, 50.00, 2, 3),
+(5, 10.00, 3, 2),
+(6, 20.00, 4, 4),
+(7, 40.00, 5, 10),
+(8, 60.00, 6, 10),
+(9, 50.00, 7, 9),
+(10, 700.00, 8, 8),
+(11, 1000.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
