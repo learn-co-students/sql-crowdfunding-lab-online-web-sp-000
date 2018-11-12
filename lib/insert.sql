48a6cacb4c9cf399c431d3f30760936a115521a2
Austin  Burke <aburk3@gmail.com>
2018-11-12 21:39:09 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,14 +1,14 @@
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
-(1, 'Ride across America', 'adventurous', 2,250.00, '2018-12-01', '2019-01-31')
-(2, 'Learn piano', 'musical', 250.00, '2018-12-30', '2019-01-30')
-(3, 'Learn to code', 'tech', 8,000.00, '2018-09-24', '2019-08-31')
-(4, 'Mountain bike Big Rock', 'adventurous', 1,000.00, '2018-01-01', '2018-07-31')
-(5, 'Build fighting robot', 'tech', 5,175.00, '2019-3-01', '2019-06-31')
-(6, 'Cat has fleas', 'charity', 150.00, '2018-12-01', '2018-12-31')
-(7, 'Cat needs more toys', 'charity', 80.00, '2018-12-01', '2018-12-24')
-(8, 'Going skydiving for New Year', 'adventurous', 1000.00, '2018-12-01', '2018-01-01')
-(9, 'Teach coding in Africa', 'charity', 5,000.00, '2019-1-01', '2019-04-31')
-(10, 'Learn to play drums', 'musical', 800.00, '2018-2-01', '2019-03-31')
+(1, 'Ride across America', 'adventurous', 2,250.00, '2018-12-01', '2019-01-31'),
+(2, 'Learn piano', 'musical', 250.00, '2018-12-30', '2019-01-30'),
+(3, 'Learn to code', 'tech', 8,000.00, '2018-09-24', '2019-08-31'),
+(4, 'Mountain bike Big Rock', 'adventurous', 1,000.00, '2018-01-01', '2018-07-31'),
+(5, 'Build fighting robot', 'tech', 5,175.00, '2019-3-01', '2019-06-31'),
+(6, 'Cat has fleas', 'charity', 150.00, '2018-12-01', '2018-12-31'),
+(7, 'Cat needs more toys', 'charity', 80.00, '2018-12-01', '2018-12-24'),
+(8, 'Going skydiving for New Year', 'adventurous', 1000.00, '2018-12-01', '2018-01-01'),
+(9, 'Teach coding in Africa', 'charity', 5,000.00, '2019-1-01', '2019-04-31'),
+(10, 'Learn to play drums', 'musical', 800.00, '2018-2-01', '2019-03-31');
 
 INSERT INTO users (id, name, age) VALUES
 (1, 'Austin', 24), (2, 'Mallory', 23), (3, 'Dylan', 26), (4, 'Justin', 26),
