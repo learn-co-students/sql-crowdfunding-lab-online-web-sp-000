0962c2d3dd7ae058ab57796456c36ff6abe89c5a
Austin  Burke <aburk3@gmail.com>
2018-11-12 20:50:31 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,3 +9,13 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 (8, 'Going skydiving for New Year', 'adventurous', 200.00, '2018-12-01', '2018-01-01')
 (9, 'Teach coding in Africa', 'charity', 5,000.00, '2019-1-01', '2019-04-31')
 (10, 'Learn to play drums', 'musical', 800.00, '2018-2-01', '2019-03-31')
+
+INSERT INTO users (id, name, age) VALUES
+(1, 'Austin', 24), (2, 'Mallory', 23), (3, 'Dylan', 26), (4, 'Justin', 26),
+(5, 'Chase', 26), (6, 'Bekka', 23), (7, 'Bryan', 27), (8, 'Ryan', 27),
+(9, 'Jason', 32), (10, 'Bradley', 24), (11, 'Jerrad', 24), (12, 'Kailey', 28),
+(13, 'Chris', 29), (14, 'Macey', 23), (15, 'Gabby', 23), (16, 'Andie', 22),
+(17, 'Rebekah', 23), (18, 'Russell', 24), (19, 'Brock', 24), (20, 'Amadaeus', 24);
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+()
