c9b92a5f6664ac94782896f2a35fd13bcfe826b2
Cookie McCormick <muntananuchat@gmail.com>
2018-12-13 18:53:12 -0500

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -2,9 +2,9 @@ CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding_goal INTEGER,
-  start_date INTEGER,
-  end_date INTEGER
+  funding_goal REAL,
+  start_date TEXT,
+  end_date TEXT
 );
 
 CREATE TABLE users (
@@ -15,7 +15,7 @@ CREATE TABLE users (
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
-  amount INTEGER,
+  amount REAL,
   user_id INTEGER,
-  project_ID INTEGER
+  project_id INTEGER
 );
