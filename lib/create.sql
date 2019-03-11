0202f74a84da76b682e84a173860479017443c5a
Tommy Curley <thomas.curley93@gmail.com>
2019-03-11 16:10:09 -0600

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+create table projects
+(id INTEGER PRIMARY KEY,
+  title text,
+  category text,
+  funding_goal float,
+  start_date text,
+  end_date text);
+
+
+create table users
+(id INTEGER PRIMARY KEY,
+  name text,
+  age integer);
+
+create table pledges
+(id INTEGER PRIMARY KEY,
+  amount float,
+  user_id integer,
+  project_id integer);
