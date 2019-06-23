e1897a0e84e0e25d141765d03db3885ba7fc8ae6
Alan Krajina <alankrajina88@gmail.com>
2019-06-23 10:01:55 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+INSERT INTO users (id, name, age) VALUES (1, "Maru", 1);
+INSERT INTO users (id, name, age) VALUES (2, "Semi", 3);
+INSERT INTO users (id, name, age) VALUES (3, "Brut", 2);
+INSERT INTO users (id, name, age) VALUES (4, "Dona", 5);
+INSERT INTO users (id, name, age) VALUES (5, "Moki", 6);
+INSERT INTO users (id, name, age) VALUES (6, "Leni", 4);
+INSERT INTO users (id, name, age) VALUES (7, "Bleki", 3);
+INSERT INTO users (id, name, age) VALUES (8, "Rex", 7);
+INSERT INTO users (id, name, age) VALUES (9, "Mis", 9);
+INSERT INTO users (id, name, age) VALUES (10, "Peso", 3);
+INSERT INTO users (id, name, age) VALUES (11, "Manki", 1);
+INSERT INTO users (id, name, age) VALUES (12, "Cupka", 1);
+INSERT INTO users (id, name, age) VALUES (13, "Bella", 4);
+INSERT INTO users (id, name, age) VALUES (14, "Isius", 9);
+INSERT INTO users (id, name, age) VALUES (15, "Riko", 7);
+INSERT INTO users (id, name, age) VALUES (16, "Oki",5);
+INSERT INTO users (id, name, age) VALUES (17, "Max", 7);
+INSERT INTO users (id, name, age) VALUES (18, "Papa", 11);
+INSERT INTO users (id, name, age) VALUES (19, "Lola", 8);
+INSERT INTO users (id, name, age) VALUES (20, "Cigo", 3);
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Zg", "gradj", 1000, 1999, 2000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, "St", "rac", 10, 1999, 2000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, "Ri", "avio", 900, 1999, 2000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, "Os", "ski", 80, 1999, 2000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, "Du", "nog", 500, 1999, 2000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (6, "Ses", "kul", 30, 1999, 2000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (7, "Vin", "turiz", 100, 1999, 2000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (8, "Ka", "more", 160, 1999, 2000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (9, "Zd", "pol", 101, 1999, 2000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (10, "Va", "suma", 140, 1999, 2000);
+
+
