6deae17e9f3ed61b6da56d9a7c989964bd51ac47
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-30 20:21:25 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,18 @@
-def create
-"CREATE TABLE project(id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);"
-end
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY, 
+title TEXT, category TEXT, 
+funding_goal INTEGER, 
+start_date TEXT, 
+end_date TEXT);
+
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER);
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount INTEGER,
+user_id INTEGER,
+project_id INTEGER
+);
