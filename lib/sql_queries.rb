0eb638c67659dc859c34b97504b779116918f1ab
Blythe Pollard <blytheepollard@gmail.com>
2019-11-08 19:34:21 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,32 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "Write your SQL query Here"
+  #How much money did each project get?
+  "SELECT Projects.title, Pledges.amount FROM Projects 
+  INNER JOIN P"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
+  #How much did each user pledge?
   "Write your SQL query Here"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
+  #For projects that met their goal, how did they do?
   "Write your SQL query Here"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
+  #How much did each user pledge total?
   "Write your SQL query Here"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
+  #Who pledged and how much was pledged in music?
   "Write your SQL query Here"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
+  #Who pledged and how much in books category?
   "Write your SQL query Here"
 end
