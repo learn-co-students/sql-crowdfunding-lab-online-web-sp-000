ad24c49eb7731480b6f0b17e92a3f46134ac6fc9
Anthony Tilelli <anthony@tilelli.me>
2019-12-29 19:36:28 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,6 +21,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
+  return "PIE"
   "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS remaining FROM projects INNER JOIN pledges ON projects.id == pledges.project_id GROUP BY projects.title HAVING remaining >= 0;"
 end
 
