bc9f847ce645471b77b887ea39ed7e84bfe41e2f
Chloe Gell <gellert.chloe@gmail.com>
2020-02-21 21:28:48 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,17 +1,11 @@
-# Write your sql queries in this file in the appropriate method like the example below:
-#
-# def select_category_from_projects
-# "SELECT category FROM projects;"
-# end
-
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "Write your SQL query Here"
+  "SELECT title, amount FROM pledges ORDER BY title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "Write your SQL query Here"
+  "SELECT user_name, age, pledge_amount FROM pledges OUTER JOIN  ON ORDER BY name; "
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
@@ -27,5 +21,9 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT category, SUM(pledges) FROM projects WHERE category = 'books';" 
 end
+
+#INSERT INTO users (id, name, age) VALUES 
+#INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+#INSERT INTO pledges (id, amount, user_id, project_id) VALUES
