acb20283dd6c0c3f16a6eb36e6ac419b72a33d11
Chloe Gell <gellert.chloe@gmail.com>
2020-02-23 23:16:24 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT project.title, project.amouontovergoal FROM projects WHERE SUM >= project.funding_goal;"
+  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS amount_over FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE amount_over >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT users.name, pledges.amount FROM pledges LEFT JOIN users ON users.id = pledges.user_id GROUP BY users.name ORDER BY pledges.amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
@@ -19,7 +19,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "SELECT category, SUM(pledges) FROM projects WHERE category = 'books';" 
+  "SELECT projects.category, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'books';" 
 end
 
 #INSERT INTO users (id, name, age) VALUES 
