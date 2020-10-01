135a86f7b6effe39e131cafb07754df372b57f89
lukejamesmorrison <lukejamesmorrison@gmail.com>
2020-10-01 16:01:52 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users
+(
+    id INTEGER PRIMARY KEY,
+    age INTEGER,
+    name TEXT
+);
+
+
+CREATE TABLE projects
+(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    funding_goal REAL,
+    start_date TEXT,
+    end_date TEXT
+);
+
+CREATE TABLE pledges
+(
+    id INTEGER PRIMARY KEY,
+    amount REAL,
+    user_id INTEGER,
+    project_id INTEGER
+);
+
