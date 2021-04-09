5c05a06700d0cacff3c98b57aa08d66ce7220bea
Carrie Jones <c.jessica.jones@gmail.com>
2021-04-09 20:11:16 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,34 @@
+INSERT INTO users VALUES 
+(1, "Abe", 45),
+(2, "Sara", 24),
+(3, "Frank", 30),
+(4, "Tara", 42),
+(5, "Lisa", 29),
+(6, "Michael", 31),
+(7, "Sean", 55),
+(8, "Bud", 71),
+(9, "Holly", 15),
+(10, "Becky", 20),
+(11, "Jake", 44),
+(12, "Megan", 27),
+(13, "Chris", 33),
+(14, "Marc", 37),
+(15, "Chelsea", 62),
+(16, "Traci", 51),
+(17, "David", 63),
+(18, "Joe", 19),
+(19, "Gerard", 22),
+(20, "Eric", 23);
+
+
+INSERT INTO projects VALUES 
+(1, "Project A", "Category A", 150000, "3/20/2020", "5/20/2021"),
+(2, "Project B", "Category B", 250000, "1/20/2021", "7/2/2021"),
+(3, "Project C", "Category C", 17000, "7/5/2021", "12/2/2021"),
+(4, "Project D", "Category D", 1000, "4/20/2021", "8/2/2021"),
+(5, "Project E", "Category D", 1500000, "9/20/2021", "1/2/2022"),
+(6, "Project F", "Category B", 5000000, "1/2/2019", "7/1/2021"),
+(7, "Project G", "Category B", 25000, "1/1/2021", "12/31/2021"),
+(8, "Project H", "Category C", 50000, "2/20/2021", "2/2/2022"),
+(9, "Project I", "Category C", 425000, "1/1/2021", "1/1/2022"),
+(10, "Project J", "Category C", 7000, "6/20/2021", "12/25/2021");
