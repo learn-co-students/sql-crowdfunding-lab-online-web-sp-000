5166d5ed5fbb0f8a6abb20874fc6efd154958050
Carrie Jones <c.jessica.jones@gmail.com>
2021-04-10 19:02:11 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -6,6 +6,6 @@ age INTEGER);
 
 CREATE TABLE pledges
 (id INTEGER PRIMARY KEY,
-amount FLOAT,
+amount INTEGER,
 user_id INTEGER,
 project_id INTEGER);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,16 +21,24 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   ORDER BY users.name ASC;"
 end
 
-def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
-end
+#def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
+#  "SELECT projects.title, projects.funding_goal- AS amount_over_goal FROM projects
+#  JOIN pledges ON projects.id = pledges.project_id
+#   WHERE SUM(pledges.amount) BETWEEN  
+#   GROUP BY pledges.project_id"
+#end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT users.name, SUM(pledges.amount) AS summed_amount FROM users
+  JOIN pledges ON users.id = pledges.user_id
+  GROUP BY users.id
+  ORDER BY summed_amount ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT * , SUM(pledges.amounts) FROM projects
+  JOIN projects ON projects.id = pledges.project_id
+  WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
